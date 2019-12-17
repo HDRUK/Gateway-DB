85124bde21e7edb1970da3b68b142bb16286ef8e
Kathryn Ellington <Kathryn.Ellington@ibm.com>
2019-12-17 15:22:24 +0000

    update syntax

--- sql/004_alter_accessrequest_dates.sql
+++ sql/004_alter_accessrequest_dates.sql
@@ -1,6 +1,6 @@
 ALTER TABLE accessrequest
     ADD accessrequest_updated_on timestamp
         with time zone DEFAULT now
-        (),
-    RENAME COLUMN accessrequest_requested_on TO accessrequest_created_on,
-    ALTER COLUMN accessrequest_project_startdate DROP DEFAULT
+        ();
+ALTER TABLE accessrequest RENAME COLUMN accessrequest_requested_on TO accessrequest_created_on;
+ALTER TABLE accessrequest ALTER COLUMN accessrequest_project_startdate DROP DEFAULT;
